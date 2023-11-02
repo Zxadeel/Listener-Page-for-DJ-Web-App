6362d5d2470e0619d787c4ae2d132e9219cce549
Adeel Waqar <zxadeel@gmail.com>
2023-11-02 00:44:41 -0400

    created route to send pref data to player

--- server.js
+++ server.js
@@ -5,15 +5,37 @@ var app = express();
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
     var username = "coolguy97"
     res.render('pages/pref.ejs', {
         username:username
     });
 });
 
-app.get('/player', function(req, res){
-    res.render('pages/player.ejs');
+app.post('/processPref', (req, res) =>{
+    const genres = req.body.Genre;
+    const emotions = req.body.Emotion;
+    const rhythms = req.body.Rhythm;
+    
+    const prefData = `${genres}, ${emotions}, ${rhythms}`;
+
+    res.redirect(`/player?prefData=${prefData}`);
+});
+
+// app.post('/processEmot', (req, res) =>{
+//     const prefData = req.body.prefData;
+//     res.redirect(`/player?prefData=${prefData}`);
+// });
+
+// app.post('/processRhyth', (req, res) =>{
+//     const prefData = req.body.prefData;
+//     res.redirect(`/player?prefData=${prefData}`);
+// });
+
+app.get('/player', (req, res) => {
+    const prefData = req.query.prefData;
+    
+    res.render('pages/player.ejs', { prefData });
 });
 
 app.listen(8080);
